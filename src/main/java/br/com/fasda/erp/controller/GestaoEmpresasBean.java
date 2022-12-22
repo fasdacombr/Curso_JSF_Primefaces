f61df30599f916a961ecf1dfc53960ed91d907e7
Flavio <flavio_125@hotmail.com>
2022-12-22 12:35:03 -0300

    Criando o Managed Bean

--- src/main/java/br/com/fasda/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/br/com/fasda/erp/controller/GestaoEmpresasBean.java
@@ -5,19 +5,28 @@ import java.io.Serializable;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import br.com.fasda.erp.model.Empresa;
+import br.com.fasda.erp.model.TipoEmpresa;
+
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private static Integer NUMERO = 0;
+	private Empresa empresa = new Empresa();
 	
-	public GestaoEmpresasBean() {
-		NUMERO++;
+	public void salvar() {
+		System.out.println("Razão social: " + empresa.getRazaoSocial()
+				+ " - Nome fantasia: " + empresa.getNomeFantasia()
+				+ " - Tipo: " + empresa.getTipo());
 	}
 	
-	public Integer getNumero() {
-		return NUMERO;
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+	
+	public TipoEmpresa[] getTipoEmpresa() {
+		return TipoEmpresa.values();
 	}
 }
